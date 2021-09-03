9124aa34c27a5b4963da7eab21ea719ce72831bc
Denis <veremyov.of@gmail.com>
2021-09-03 09:34:44 +0300

    add basket scss

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -5,20 +5,30 @@ export default function Basket() {
 
     const dispatch = useDispatch();
     const basket = useSelector(state => state.basket.basket);
+    const basketSum = useSelector(state => state.basketSum.basketSum)
     return (
-        <div>
-            <h2>Корзина</h2>
-            <div>
+        <div className="basket">
+            <h2 className="basket_title">Корзина</h2>
+            <div className="basket_wrapp">
                 {basket.map((item, index) => (
-                    <div>
-
+                    <div className="basket_wrapp_item">
+                        <div className="basket_wrapp_item_inner">
+                            <div className="basket_wrapp_item_inner_img"><img src={item.imgUrl}/></div>
+                            <h3 className="basket_wrapp_item_inner_name">{item.name}</h3>
+                            <div className="basket_wrapp_item_inner_size">{item.size ? '40 см' : '30 см'}</div>
+                        </div>
+                        <div className="basket_wrapp_item_funBlock">
+                            <div className="basket_wrapp_item_funBlock_price">{item.price} руб</div>
+                            <div></div>
+                            <button className="basket_wrapp_item_funBlock_close"></button>
+                        </div>
                     </div>
                 ))}
-                <div>Общая сумма:<span></span></div>
+                <div className="sum">Общая сумма:<span> {basketSum}руб</span></div>
             </div>
-            <div>
-                <Link to="/">Вернуться в меню</Link>
-                <button>Оформить заказ</button>
+            <div className="basket_group">
+                <Link className="basket_group_link" to="/">Вернуться в меню</Link>
+                <button className="basket_group_btn">Оформить заказ</button>
             </div>
         </div>
     );
